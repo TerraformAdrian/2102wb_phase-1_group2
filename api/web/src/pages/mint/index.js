65a8fdee0ae31031a6bd74dec47122914118fb25
smart-window <smart.free.dev@gmail.com>
2021-11-11 07:39:46 -0700

    Update

---
+++ api/web/src/pages/mint/index.js
@@ -0,0 +1,63 @@
+
+import {IDLE} from "../global/constants"
+import {useCurrentUser} from "../hooks/use-current-user.hook"
+import {useAccountItems} from "../hooks/use-account-items.hook"
+import {Suspense, useState} from "react"
+import {Redirect, useHistory} from "react-router-dom"
+import AccountItemsCluster from '../comps/account-items'
+
+export function Page() {
+  const [state, setState] = useState({
+    txtAddress: ""
+  })
+  const [address, setAddress] = useState("");
+  const [count, setCount] = useState(0);
+  const history = useHistory();
+  const [user] = useCurrentUser()
+
+  // if (user.addr == null) return <Redirect to={"/"} />
+
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleList = (e) => {
+
+    if (state.txtAddress.length != 18) {
+      alert("Enter Address Corretly!");
+      return;
+    }
+
+    setAddress(state.txtAddress);
+    setCount(1 - count);
+  }
+
+  const handleMint = (e) => {
+    e.preventDefault();
+    history.push("/publish");
+  }
+
+  const handleMarket = (e) => {
+    e.preventDefault();
+    history.push("/market");
+  }
+
+  return (
+    <div>
+      <h1>NFT Listings</h1>
+	    <div>
+        <label>Address: </label>
+        <input name="txtAddress" id="txtAddress" onChange={handleChange}/>&nbsp;
+        <button onClick={handleList}>List NFTs</button>
+        &nbsp;<button onClick={handleMint}>Mint an NFT</button>
+        &nbsp;<button onClick={handleMarket}>View Marketplace</button>
+        {
+          address != "" && <AccountItemsCluster address={address} />
+        }
+	    </div>
+    </div>
+  )
+}

116a4f82e3351b8644267dbea96e58311e001570
smart-window <smart.free.dev@gmail.com>
2022-01-29 23:00:33 +0000

    setup.sh edited online with Bitbucket

--- .ebextensions/bin/setup.sh
+++ .ebextensions/bin/setup.sh
@@ -1,15 +1,7 @@
-wall "/var/app/current/.ebextensions/bin/setup.sh::  starting file"
-
 # change path
 cd /var/app/current/
 
 # NPM make build
 npm run build
-wall "/var/app/current/.ebextensions/bin/setup.sh::  starting file"
-wall "/var/app/current/.ebextensions/bin/setup.sh::  ran cmd =  npm run build"
-
-# NPM start app
-npm run start
-wall "/var/app/current/.ebextensions/bin/setup.sh::  starting file"
-wall "/var/app/current/.ebextensions/bin/setup.sh::  ran cmd =  npm run build"
-wall "/var/app/current/.ebextensions/bin/setup.sh::  ran cmd =  npm run start"
+# npm run start
+npm run dev

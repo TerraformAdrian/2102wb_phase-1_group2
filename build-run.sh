f3df8d6ad49a5a2a72b47b7a1da93a8e6d23475b
smart-window <smart.free.dev@gmail.com>
2022-01-30 21:53:31 +0000

    build-run.sh edited online with Bitbucket

--- build-run.sh
+++ build-run.sh
@@ -4,12 +4,15 @@
 pwd
 
 # sudo ls >> /home/ec2-user/dir.txt
-sudo date >> /home/ec2-user/date.txt
+sudo date >> /home/ec2-user/build-run.txt
 
 # change path
 cd /var/app/current/
 
 # NPM make build
+wall /var/app/current/npm-run: building
 npm run build
+wall /var/app/current/npm-run: starting
 npm run start
+wall /var/app/current/npm-run: running....
 #npm run dev

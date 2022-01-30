20950a531571645ba0c2f4d83de05047a76cf37d
smart-window <smart.free.dev@gmail.com>
2022-01-30 21:07:17 +0000

    setup.sh edited online with Bitbucket

--- .ebextensions/setup.sh
+++ .ebextensions/setup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# sudo ls >> /home/ec2-user/dir.txt
+sudo date >> /home/ec2-user/date.txt
+
 # change path
 cd /var/app/current/
 
 # NPM make build
 npm run build
-# npm run start
-npm run dev
+npm run start
+#npm run dev
